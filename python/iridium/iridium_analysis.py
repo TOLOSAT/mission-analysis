b0fafb78934f29d2fceff1a96549b3e34e204132
Arnaud Muller <arnaud.muller1308@gmail.com>
2023-03-08 11:49:59 +0100

    Updated dark plots

--- python/iridium/iridium_analysis.py
+++ python/iridium/iridium_analysis.py
@@ -8,6 +8,18 @@ from iridium_doppler import (
 
 from useful_functions import plot_functions as pf
 
+fig, axes = pf.dark_figure()
+axes[0].plot(
+    IRIDIUM_sat_results["seconds"] / 86400, IRIDIUM_sat_results["doppler_shift"] / 1e3
+)
+axes[0].set_title(f"Doppler shift of {selected_iridium}")
+axes[0].set_xlabel("Time since launch [days]")
+axes[0].set_ylabel("Doppler shift [kHz]")
+axes[0].set_xlim(0, IRIDIUM_sat_results["seconds"].max() / 86400)
+pf.finish_dark_figure(
+    fig, f"results/{selected_iridium_nospace}_doppler_shift.png", show=True
+)
+
 fig, axes = pf.light_figure()
 axes[0].plot(
     IRIDIUM_sat_results["seconds"] / 86400, IRIDIUM_sat_results["doppler_shift"] / 1e3
@@ -20,6 +32,18 @@ pf.finish_light_figure(
     fig, f"results/{selected_iridium_nospace}_doppler_shift_light.png", show=True
 )
 
+fig, axes = pf.dark_figure()
+axes[0].plot(
+    IRIDIUM_sat_results["seconds"] / 86400, IRIDIUM_sat_results["doppler_rate"]
+)
+axes[0].set_title(f"Doppler rate of {selected_iridium}")
+axes[0].set_xlabel("Time since launch [days]")
+axes[0].set_ylabel("Doppler rate [Hz/s]")
+axes[0].set_xlim(0, IRIDIUM_sat_results["seconds"].max() / 86400)
+pf.finish_dark_figure(
+    fig, f"results/{selected_iridium_nospace}_doppler_rate.png", show=True
+)
+
 fig, axes = pf.light_figure()
 axes[0].plot(
     IRIDIUM_sat_results["seconds"] / 86400, IRIDIUM_sat_results["doppler_rate"]
@@ -30,6 +54,21 @@ axes[0].set_ylabel("Doppler rate [Hz/s]")
 axes[0].set_xlim(0, IRIDIUM_sat_results["seconds"].max() / 86400)
 pf.finish_light_figure(
     fig, f"results/{selected_iridium_nospace}_doppler_rate_light.png", show=True
+)
+
+fig, axes = pf.dark_figure()
+axes[0].plot(
+    IRIDIUM_visibility["seconds"] / 86400,
+    IRIDIUM_visibility["sum_ok"],
+    linestyle="none",
+    marker=".",
+)
+axes[0].set_title("Visibility of IRIDIUM satellites satisfying all 4 conditions")
+axes[0].set_xlabel("Time since launch [days]")
+axes[0].set_ylabel("Number of satellites [-]")
+axes[0].set_xlim(0, IRIDIUM_visibility["seconds"].max() / 86400)
+pf.finish_dark_figure(
+    fig, "results/IRIDIUM_visibility.png", show=True, force_y_int=True
 )
 
 fig, axes = pf.light_figure()
@@ -47,6 +86,15 @@ pf.finish_light_figure(
     fig, "results/IRIDIUM_visibility_light.png", show=True, force_y_int=True
 )
 
+fig, axes = pf.dark_figure()
+axes[0].vlines(IRIDIUM_windows["seconds"] / 86400, 0, IRIDIUM_windows["duration"] / 60)
+axes[0].set_title("Visibility windows of at least one IRIDIUM satellite")
+axes[0].set_xlabel("Time since launch [days]")
+axes[0].set_ylabel("Window duration [mins]")
+axes[0].set_ylim(0, IRIDIUM_windows["duration"].max() / 60)
+axes[0].set_xlim(0, IRIDIUM_windows["seconds"].max() / 86400)
+pf.finish_dark_figure(fig, "results/IRIDIUM_windows.png", show=True)
+
 fig, axes = pf.light_figure()
 axes[0].vlines(IRIDIUM_windows["seconds"] / 86400, 0, IRIDIUM_windows["duration"] / 60)
 axes[0].set_title("Visibility windows of at least one IRIDIUM satellite")
@@ -55,29 +103,3 @@ axes[0].set_ylabel("Window duration [mins]")
 axes[0].set_ylim(0, IRIDIUM_windows["duration"].max() / 60)
 axes[0].set_xlim(0, IRIDIUM_windows["seconds"].max() / 86400)
 pf.finish_light_figure(fig, "results/IRIDIUM_windows_light.png", show=True)
-
-# mask = (results_dict["timedelta"] > timedelta(hours=2310, minutes=13)) & (
-#         results_dict["timedelta"] <= timedelta(hours=2310, minutes=27))
-# fig, axes = pf.dark_figure()
-# axes[0].plot(results_dict["timedelta"].loc[mask].dt.total_seconds(),
-#              results_dict["IRIDIUM 113"].loc[mask, "doppler_shift"].to_list(), linestyle="none",
-#              marker=".")
-# axes[0].set_title("Doppler shift of IRIDIUM 113")
-# axes[0].set_xlabel("Time [seconds]")
-# axes[0].set_ylabel("Doppler shift [Hz]")
-# axes[0].set_xlim(min(results_dict["timedelta"].loc[mask].dt.total_seconds()),
-#                  max(results_dict["timedelta"].loc[mask].dt.total_seconds()))
-# pf.finish_dark_figure(fig, "IRIDIUM_all_ok_1day_doppler_shift.png", show=True)
-
-# mask = (results_dict["timedelta"] > timedelta(hours=2325, minutes=10)) & (
-#         results_dict["timedelta"] <= timedelta(hours=2335, minutes=13))
-# fig, axes = pf.dark_figure()
-# axes[0].plot(results_dict["timedelta"].loc[mask].dt.total_seconds(),
-#              IRIDIUM_visibility.loc[mask, "sum_ok"].to_list(), linestyle="none",
-#              marker=".")
-# axes[0].set_title("Visibility of IRIDIUM satellites satisfying all 4 conditions")
-# axes[0].set_xlabel("Time [seconds]")
-# axes[0].set_ylabel("Number of satellites [-]")
-# axes[0].set_xlim(min(results_dict["timedelta"].loc[mask].dt.total_seconds()),
-#                  max(results_dict["timedelta"].loc[mask].dt.total_seconds()))
-# pf.finish_dark_figure(fig, "IRIDIUM_all_ok_1day.png", show=True)
