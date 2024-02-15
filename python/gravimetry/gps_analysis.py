dda69a804111a08ff9beef1bcdee15d661aaa9ac
Umberto di Capua <umberto.di-capua@student.isae-supaero.fr>
2024-02-15 19:11:19 +0100

    Update gps_analysis.py

--- python/gravimetry/gps_analysis.py
+++ python/gravimetry/gps_analysis.py
@@ -7,17 +7,21 @@ from gps_doppler import (
 )
 
 from useful_functions import plot_functions as pf
+from useful_functions import datetime_to_epoch
 
 import pandas as pd
 
-desired_hour_start = 1  # Hours past after the start of the propagation
-desired_window_duration = 2  # Hours
+desired_start_date = "2024-01-02"
+desired_start_time = "13:00:00"
+desired_end_date = "2024-01-03"
+desired_end_time = "13:00:00"
 
-window_start_seconds = gps_sat_results["seconds"][0] + desired_hour_start * 3600
-window_end_seconds = window_start_seconds + desired_window_duration * 3600
+window_start_seconds = datetime_to_epoch(f"{desired_start_date} {desired_start_time}")
+window_end_seconds = datetime_to_epoch(f"{desired_end_date} {desired_end_time}")
+
 # Find the index of the window that contains the desired time
-window_index_start = gps_sat_results["seconds"].searchsorted(window_start_seconds)
-window_index_end = gps_sat_results["seconds"].searchsorted(window_end_seconds)
+window_index_start = gps_sat_results["epochs"].searchsorted(window_start_seconds)
+window_index_end = gps_sat_results["epochs"].searchsorted(window_end_seconds)
 
 gps_sat_results_window = pd.DataFrame(columns=[])
 
@@ -25,10 +29,10 @@ gps_sat_results_window = gps_sat_results.iloc[window_index_start:window_index_en
 
 fig, axes = pf.dark_figure()
 axes[0].plot(gps_sat_results_window["seconds"] / 86400, gps_sat_results_window["doppler_shift"] / 1e3)
-axes[0].set_title(f"Doppler shift of {selected_gps} from {desired_hour_start} hours after launch to {desired_hour_start + desired_window_duration} hours after launch")
+axes[0].set_title(f"Doppler shift of {selected_gps}")
 axes[0].set_xlabel("Time since launch [days]")
 axes[0].set_ylabel("Doppler shift [kHz]")
-axes[0].set_xlim(gps_sat_results_window["seconds"].min() / 86400, gps_sat_results_window["seconds"].max() / 86400)
+axes[0].set_xlim(window_start_seconds / 86400, window_end_seconds / 86400)
 pf.finish_dark_figure(
     fig, f"results/{selected_gps_nospace}_doppler_shift.png", show=True
 )
